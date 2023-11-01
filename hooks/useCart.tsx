b3d2dc4f34013ff13e10424a325f048ad6a235be
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-01 16:33:39 +0700

    Update useCart.tsx

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
-import { product } from "@/utils/product";
+
 import { createContext,useCallback,useContext,useEffect,useState } from "react";
 import toast, { Toast } from "react-hot-toast";
 
 type CartContextType ={
-    cartTotalQty:number
+    cartTotalQty:number;
+    cartTotalAmount: number;
     cartProducts:CartProductType[] | null;
     handleAddProductToCart:(product:CartProductType)=> void
     handleRemoveProductFromCart:(product:CartProductType)=> void
@@ -17,10 +18,13 @@ type CartContextType ={
 export const CartContext =  createContext<CartContextType|null>(null);
 interface Props{
     [propName:string]:any;
-}export const CartContextProvider = (props: Props) => {
-    const [cartTotalQty, setCartTotalQty] = useState(10);
+}
+export const CartContextProvider = (props: Props) => {
+    const [cartTotalQty, setCartTotalQty] = useState(0);
+    const[cartTotalAmount,setCartTotalAmount]=useState(0)
     const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null);
-
+    console.log('qty',cartTotalQty)
+    console.log('amount',cartTotalAmount)
     // Initialize cart products from local storage when the component mounts.
     useEffect(() => {
         const cartItems: any = localStorage.getItem('eShopCartItems');
@@ -28,6 +32,27 @@ interface Props{
 
         setCartProducts(cProducts);
     }, []);
+    
+    useEffect(()=>{
+        const getTotals =()=>{
+            if(cartProducts){
+            const{ total,qty}=cartProducts?.reduce((acc,item)=>{
+                const itemTotal = item.price * item.quantity
+                acc.total += itemTotal
+                acc.qty += item.quantity
+                return acc;
+            },
+            {
+                total:0,
+                qty:0
+            }
+            );
+        setCartTotalQty(qty);
+       setCartTotalAmount(total);
+        }};
+        getTotals()
+    },[cartProducts])
+
 
     // Define a function to add a product to the cart.
     const handleAddProductToCart = useCallback((product: CartProductType) => {
@@ -99,6 +124,7 @@ interface Props{
     // Provide the cart context value to consumers.
     const value = {
         cartTotalQty,
+        cartTotalAmount,
         cartProducts,
         handleAddProductToCart,
         handleRemoveProductFromCart,
