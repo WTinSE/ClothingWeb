9ebb67828edd155a5cc33e9191a48f88fc796c1b
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-01 19:27:46 +0700

    Update Footer.tsx

--- app/ components/footer/Footer.tsx
+++ app/ components/footer/Footer.tsx
@@ -10,33 +10,34 @@ const Footer  = () => {
             <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
                 <FooterList>
                     <h3 className="text-base font-bold
-                    mb-2">Shop Categories</h3>
-                    <Link href='#'>Quan Dai</Link>
-                    <Link href='#'>Ao so mi</Link>
-                    <Link href='#'>Ao thun</Link>
-                    <Link href='#'>Ao khoat</Link>
-                    <Link href='#'>Quan sort</Link>
-                    <Link href='#'>Phu kien</Link>
+                    mb-2">Danh mục cửa hàng</h3>
+                    <Link href='#'>Quần dài</Link>
+                    <Link href='#'>Áo sơ mi</Link>
+                    <Link href='#'>Áo thun</Link>
+                    <Link href='#'>Áo khoát</Link>
+                    <Link href='#'>Quần sort</Link>
+                    <Link href='#'>Phụ kiện</Link>
                 </FooterList>
                 <FooterList>
                     <h3 className="text-base font-bold
-                    mb-2">Customer Sevice</h3>
-                    <Link href='#'>Contact Us</Link>
-                    <Link href='#'>Shipping Policy</Link>
-                    <Link href='#'>Return & Exchanges</Link>
-                    <Link href='#'>Watches</Link>
-                    <Link href='#'>FAQs</Link>
+                    mb-2">Dịch vụ khách hàng</h3>
+                    <Link href='#'>Liên hệ chúng tôi</Link>
+                    <Link href='#'>Chính sách vận hành</Link>
+                    <Link href='#'>Chính sách đổi trả và hoàn tiền</Link>
+                    <Link href='#'>Trang sức</Link>
+                    <Link href='#'>Câu hỏi thường gặp</Link>
                 </FooterList>
                
                     <div className="w-full md:w-1/3 mb-6 md:mb-0">
-                        <h3 className="text-base font-bold mb-2">About Us</h3>
+                        <h3 className="text-base font-bold mb-2">Giới thiệu về chúng tôi</h3>
                         <p className="mb-2">
                         "Chào mừng bạn đến với cửa hàng thời trang của chúng tôi, nơi chúng tôi tự hào giới thiệu bộ sưu tập đa dạng với các loại trang phục từ áo sơ mi thanh lịch đến quần jean cá tính và váy dự tiệc lộng lẫy. Chúng tôi cam kết mang đến cho bạn sự lựa chọn phong phú để bạn có thể thể hiện phong cách riêng mình mỗi ngày. Đội ngũ chuyên nghiệp của chúng tôi sẵn sàng tư vấn và hỗ trợ bạn trong việc tìm kiếm sản phẩm phù hợp nhất. Hãy tham gia chúng tôi trên hành trình thời trang đầy màu sắc và sáng tạo."
                         </p>
-                        <p>&copy;{new Date().getFullYear()} E~Shop. All rights reserved</p>
+                        <p>&copy;{new Date().getFullYear()} LuxeGlobal.Bảo lưu mọi quyền.</p>
+
                     </div>
                     <FooterList>
-                    <h3 className="text-base font-bold mb-2">Follow Us</h3>
+                    <h3 className="text-base font-bold mb-2">Kết nối với chúng tôi</h3>
                     <div className="flex gap-2">
                         <Link href="#">
                             <MdFacebook size ={24}/>
