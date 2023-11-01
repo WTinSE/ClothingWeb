c8c246d9b10acb010446c36e9917e104970c3440
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-01 15:44:12 +0700

    Update CartClient.tsx

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -8,7 +8,7 @@ import ItemContent from "./ItemContent";
 
 
 const CartClient = () => {
-    const {cartProducts} = useCart();
+    const {cartProducts,handleClearCart} = useCart();
 
     if(!cartProducts || cartProducts.length ===0)
     {
@@ -51,7 +51,7 @@ const CartClient = () => {
                 </div>
                 <div className="border-t-[1.5px] border-slate-200 py-4 flex justify-between gap-4">
                     <div className="w-[90px]">
-                        <Button label="Clear Cart" onClick={()=>{
+                        <Button label="Clear Cart" onClick={()=>{handleClearCart()
                         }} small outline />
                     </div>
                     <div className="text-sm flex flex-col gap-1 items-start">
