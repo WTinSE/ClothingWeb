8179390ae3c59bee6924343e40ab7cc3f6a877ac
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-02 12:17:22 +0700

    Create RegisterForm.tsx

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useState } from "react";
+import Heading from "../ components/Heading";
+import Input from "../ components/inputs/Input";
+import { FieldValues, useForm ,SubmitHandler} from "react-hook-form";
+import Button from "../ components/Button";
+import Link from "next/link";
+
+
+
+const RegisterForm = () => {
+    const [isLoading,setIsLoading]=useState(false)
+    const {register,handleSubmit,formState:{errors},} = useForm<FieldValues>({defaultValues:{
+        name: "", 
+        email: "",
+        password: "",
+    }});
+    const onSubmit:SubmitHandler<FieldValues>=(data)=>{setIsLoading(true);
+    console.log(data);}
+    return (  <>
+    <Heading title="Đăng ký thành viên LuxeGlobal"/>
+    <hr className="bg-slate-300 w-full h-px"/>
+    <Input id="Name"
+    label="Tên"
+    disabled={isLoading} 
+    register={register}
+    errors={errors}
+    required/>
+    <Input id="email"
+    label="Email"
+    disabled={isLoading} 
+    register={register}
+    errors={errors}
+    required/>
+    <Input id="Password"
+    label="Mật khẩu"
+    disabled={isLoading} 
+    register={register}
+    errors={errors}
+    required
+    type="password"/>
+    <Button label={isLoading?"Loading":'Dang ky'} onClick={handleSubmit(onSubmit)}/>
+    <p className="text-sm">Already have an account?{""} <Link className="underline" href={'/login'}>Log in
+    </Link>
+        </p>
+    </>);
+}
+ 
+export default RegisterForm;
