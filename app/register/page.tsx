0e872681494234794388408e48fc1e8b41be545a
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-04 07:04:05 +0700

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,14 @@
+import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "../ components/Container";
 import FormWrap from "../ components/products/FormWrap";
 import RegisterForm from "./RegisterForm";
 
 
-const Register = () => {
-    return (<Container>
+const Register = async () => {
+  const currentUser = await getCurrentUser();
+      return (<Container>
         <FormWrap>
-          <RegisterForm/>
+          <RegisterForm currentUser={currentUser}/>
         </FormWrap>
     </Container>  );
 }
