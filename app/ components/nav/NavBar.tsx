c85e44d40b1968e3d70a65ea15bad0e466fee0fc
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-04 07:03:57 +0700

    Update NavBar.tsx

--- app/ components/nav/NavBar.tsx
+++ app/ components/nav/NavBar.tsx
@@ -3,9 +3,13 @@ import  Container  from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
+import { getCurrentUser } from "@/actions/getCurrentUser";
  const redressed = Redressed({subsets:["latin"],weight:['400']});
 
-const NavBar = () => {
+const NavBar = async () => {
+
+  const currentUser = await getCurrentUser();
+
   return ( 
   <div
   className="
@@ -28,7 +32,7 @@ const NavBar = () => {
           <div className="hidden md:block">Tìm kiếm</div>
           <div className="flex items-center gap-8 md:gap-12">
            <CartCount />
-            <div className={`${redressed.className} font-bold text-2x;`}><UserMenu/></div>
+            <UserMenu currentUser={currentUser}/>
           </div>
         </div>
       </Container>
