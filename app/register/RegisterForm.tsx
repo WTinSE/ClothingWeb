ad0ffcb5623168ac8ef7a2a81f97246880b7cba4
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-04 07:04:07 +0700

    Update RegisterForm.tsx

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ components/Heading";
 import Input from "../ components/inputs/Input";
 import { FieldValues, useForm ,SubmitHandler} from "react-hook-form";
@@ -11,7 +11,12 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { signIn} from "next-auth/react";
 import { useRouter } from "next/navigation";
-const RegisterForm = () => {
+import { SafeUser } from "@/types";
+interface RegisterFormProps
+{
+    currentUser:SafeUser |null;
+}
+const RegisterForm:React.FC<RegisterFormProps> = ({currentUser}) => {
     const [isLoading,setIsLoading]=useState(false)
     const {register,handleSubmit,formState:{errors},} = useForm<FieldValues>({defaultValues:{
         name: "", 
@@ -20,6 +25,9 @@ const RegisterForm = () => {
     }});
 
     const router = useRouter()
+    useEffect(()=>{if(currentUser){router.push('/cart');
+  router.refresh();
+}},[]);
 
     const onSubmit:SubmitHandler<FieldValues>=(data)=>{setIsLoading(true);
     setIsLoading(true);
@@ -46,16 +54,20 @@ const RegisterForm = () => {
             setIsLoading(false)
         })
     };
+    if(currentUser)
+    {
+        return <p className="text-center">Logged in. Redirecting...</p>
+    }
     return (  <>
     <Heading title="Đăng ký thành viên LuxeGlobal"/>
     <Button 
     outline
     label="Đăng nhập với Google"
     icon={AiOutlineGoogle}
-    onClick={()=>{ }}/>
+    onClick={() => {signIn('google') }}/>
     <hr className="bg-slate-300 w-full h-px"/>
     <Input id="name"
-    label="Tên người dùng"
+    label="Name"
     disabled={isLoading} 
     register={register}
     errors={errors}
@@ -67,7 +79,7 @@ const RegisterForm = () => {
     errors={errors}
     required/>
     <Input id="password"
-    label="Mật khẩu"
+    label="Password"
     disabled={isLoading} 
     register={register}
     errors={errors}
