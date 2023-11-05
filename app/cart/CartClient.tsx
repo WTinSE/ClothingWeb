bd123064c561762456af233ebfe4381d0bc7c62c
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-05 22:03:12 +0700

    Update CartClient.tsx

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -6,10 +6,18 @@ import Heading from "../ components/Heading";
 import Button from "../ components/Button";
 import ItemContent from "./ItemContent";
 import { formatPrice } from "@/utils/formatPrice";
+import { SafeUser } from "@/types";
+import Checkout from "../checkout/page";
+import { useRouter } from "next/navigation";
 
+interface CartClientProps{
+    currentUser: SafeUser|null;
+}
 
-const CartClient = () => {
+const CartClient:React.FC<CartClientProps> = ({currentUser}) => {
     const {cartProducts,handleClearCart,cartTotalAmount} = useCart();
+
+    const router=useRouter()
 
     if(!cartProducts || cartProducts.length ===0)
     {
@@ -61,8 +69,10 @@ const CartClient = () => {
                             <span>{formatPrice(cartTotalAmount)}</span>
                             </div>
                             <p className="text-slate-500">Taxes and shiping calculate at checkout</p>
-                            <Button label="Check out" onClick={()=>{
-
+                            <Button label={currentUser? 'Checkout':"Login To Checkout" } 
+                            outline={currentUser? false:true}
+                            onClick={()=>{
+                                currentUser? router.push('/checkout'):router.push('/login');
                             }}/>
                             <Link  href={"/"} className="text-slate-500 flex items-center gap-1 mt-2">
                 <MdArrowBack/>
