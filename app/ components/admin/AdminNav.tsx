bd1778593eae5295c026927a9fe24e4fdea3a67b
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-06 03:23:58 +0700

    Create AdminNav.tsx

---
+++ app/ components/admin/AdminNav.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { Container } from "@mui/material";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { MdDashboard, MdDns, MdFormatListBulleted, MdLibraryAdd } from "react-icons/md";
+import AdminNavItem from "./AdminNavItem";
+
+const AdminNav = () => {
+    const pathname=usePathname()
+    return ( <div className="w-full shadow-sm top-20 border-b-[1px] pt-4">
+
+        <Container>
+            <div className="flex flex-row items-center justify-between md:justify-center gap-8 md:gap-12 overflow-x-auto flex-nowrap">
+        <Link href='/admin'> 
+        <AdminNavItem label="Summary" icon={MdDashboard} selected={pathname=="/admin"}/></Link>
+        <Link href='/admin/add-products'>
+        <AdminNavItem label="AddProduct" icon={MdLibraryAdd} selected={pathname=="/admin/add-products"}/></Link>
+        <Link href='/admin/manage-products'>
+        <AdminNavItem label="ManageProducts" icon={MdDns} selected={pathname=="/admin/manage-products"}/></Link>
+        <Link href='/admin/manage-orders'>
+        <AdminNavItem label="Manageorders" icon={MdFormatListBulleted} selected={pathname=="/admin/manage-orders"}/></Link>
+            </div>
+        </Container>
+    </div> );
+}
+ 
+export default AdminNav;
