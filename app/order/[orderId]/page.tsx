2a1e2a2c79b53a065be5219f0a6a02b4096ecb90
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2023-11-11 14:19:32 +0700

    Create page.tsx

---
+++ app/order/[orderId]/page.tsx
@@ -0,0 +1,25 @@
+import Container from "@/app/ components/Container";
+
+import ListRating from "@/app/product/[productId]/ListRating";
+import getOrderById from "@/actions/getOrderById";
+import NullData from "@/app/ components/NullData";
+import OrderDetails from "./OrderDetails";
+
+interface IPrams{
+    orderId?:string;
+}
+
+const Order= async({params}:{params:IPrams}) => {
+    const order=await getOrderById(params);
+    if(!order)return 
+    <NullData  title="No order"></NullData>
+    return ( 
+        <div className="p-8">
+            <Container>
+               <OrderDetails order={order}/>
+            </Container>
+        </div>
+     );
+}
+ 
+export default Order;
